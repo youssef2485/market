3060c6ae25f4d97827348f4219dc8861ea4a8334
youssef2485 <youssefyaiche45@gmail.com>
2023-10-25 19:36:58 +0100

    market

---
+++ src/main/java/tn/csf/market/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package tn.csf.market.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tn.csf.market.model.Article;
+
+//repository that extends CrudRepository  
+public interface ArticleRepository extends CrudRepository<Article, Integer>  
+{  
+}  
